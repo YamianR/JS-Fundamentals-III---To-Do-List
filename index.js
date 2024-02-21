4ca4847f41d4682f2d8798e9ec7928da7f86e89b
YamianR <riverayamian@gmail.com>
2024-02-21 13:39:48 -0700

    Added JS foundation to add items to the list ->WIP<-

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log('My code is running');
+let addToDoButton = document.getElementById('addToDo');
+let toDoBox = document.getElementById('toDoBox');
+let inputField = document.getElementById('inputField');
+
+addToDoButton.addEventListener('click', function() {
+    var listItem = document.createElement('li');
+    listItem.classList.add('paragraph-styling');
+    listItem.innerText = inputField.value;
+    toDoBox.appendChild(listItem);
+    inputField.value = "";
+})
